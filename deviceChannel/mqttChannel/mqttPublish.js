500dee6239118dd44628905ef37ce56d5ec7aa44
unknown <sahafsahriar@gmail.com>
2025-08-12 20:37:04 +0330

    mqtt publish edited

--- deviceChannel/mqttChannel/mqttPublish.js
+++ deviceChannel/mqttChannel/mqttPublish.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const {mqttSimulator} = require('./simulator')
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 try {
   mqttSimulator();
-  console.log('MQTT Simulator started.');
+  console.log('MQTT Simulator started');
 } catch (err) {
   console.error('Failed to start MQTT Simulator:', err);
 }
